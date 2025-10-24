68a801e2e28045906df60f332dbfcc138c0c6b0a
karthik5033 <karthikkp5033@gmail.com>
2025-10-24 23:06:16 +0530

    g

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,6 +2,7 @@ export default function ProfilePage() {
     return (
         <div className=" justify-center items-center flex flex-col min-h-screen py-2 ">
             <h1>Profile</h1>
+            <h2>page</h2>
             <br />
             <p>profle page</p>
         </div>
